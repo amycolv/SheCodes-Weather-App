48add0acdcc6aea39f78a41d9f1a14cde10434e3
Amy Colville <amy.colville@yoti.com>
2021-01-26 18:24:49 +0000

    Showing current temperature

---
+++ src/app.js
@@ -0,0 +1,21 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+let apiKey = "70926df286833f646f0c1c5d64d6c1aa";
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
