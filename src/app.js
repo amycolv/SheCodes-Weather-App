02c1c0577fed950ba797163373cb2299381bcee1
Amy Colville <amy.colville@yoti.com>
2021-01-31 09:52:55 +0000

    Fixed bug with forecast max/min temps

--- src/app.js
+++ src/app.js
@@ -77,7 +77,7 @@ function displayForecast(response) {
         class="forecast-image"
       />
     <div class="weather-forecast-temperature">
-    <strong>${Math.round(forecast.main.temp_max)}°</strong> ${Math.round(
+    <strong>${Math.round(forecast.main.temp_max)}°</strong> | ${Math.round(
       forecast.main.temp_min
     )}°
    </div>
